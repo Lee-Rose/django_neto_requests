d4990056917fe708d25ba82df1a434d1c098ca80
Lee <84514310+Lee-Rose@users.noreply.github.com>
2021-11-18 20:13:16 +0200

    first

---
+++ requests/recipes/calculator/views.py
@@ -0,0 +1,64 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.urls import reverse
+
+DATA = {
+    'omlet': {
+        'яйца, шт': 2,
+        'молоко, л': 0.1,
+        'соль, ч.л.': 0.5,
+    },
+    'pasta': {
+        'макароны, г': 0.3,
+        'сыр, г': 0.05,
+    },
+    'buter': {
+        'хлеб, ломтик': 1,
+        'колбаса, ломтик': 1,
+        'сыр, ломтик': 1,
+        'помидор, ломтик': 1,
+    },
+    # можете добавить свои рецепты ;)
+}
+
+def get_recipe(recipe, servings=1):
+    ''' get recipe in calculated portions '''
+    context_calc = {}
+    for item, values in recipe.items():
+        for i, count in values.values():
+            context_calc[i] = round((count) * servings, 3)
+    return context_calc
+
+def index_view(request, recipe):
+
+    servings = request.GET.get('servings', int(1))
+    for rec in DATA.keys():
+        if recipe == rec:
+            if int(servings) > 1 :
+                recipe = get_recipe(DATA[recipe], int(servings))
+            else:
+                recipe = get_recipe(DATA[recipe])
+            context = {'recipe': recipe, 'servings': servings}
+        else:
+            pass
+
+    return render(request, 'calculator/index.html', context=context)
+
+def all_recipe(request):
+
+    return HttpResponse(content=DATA)
+
+def home_view(request):
+
+    return HttpResponse(f"<a href={reverse('all_rec')}> <h1>Рецепты</a4> </h1>")
+
+
+# Напишите ваш обработчик. Используйте DATA как источник данных
+# Результат - render(request, 'calculator/index.html', context)
+# В качестве контекста должен быть передан словарь с рецептом:
+# context = {
+#   'recipe': {
+#     'ингредиент1': количество1,
+#     'ингредиент2': количество2,
+#   }
+# }
