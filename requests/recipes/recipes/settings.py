400e238be275f89452e205884b662447b36a4149
Lee <84514310+Lee-Rose@users.noreply.github.com>
2022-02-01 19:04:17 +0200

    fixed3

--- requests/recipes/recipes/settings.py
+++ requests/recipes/recipes/settings.py
@@ -20,12 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5q*3(1@)4kzwt@wl0f&(2mvoj0=mcatp5#6td6g)=z7g47ek*w'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+SECRET_KEY = os.getenv('SECRET_KEY', default='the-best-secret-key')
+DEBUG = os.getenv('DEBUG', default='True') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
